b3a1ba88870a1abee317430ac81474ada71f4b0b
IoanaRusu133 <124049437+IoanaRusu133@users.noreply.github.com>
2025-08-14 21:13:10 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,6 @@
+#Python Project No. 1
+#Full Adress Generator
+street = input("What is the name of your street? (eg. Riverwood, Liberty)")
+city = input("What is the name of your city? (eg. Liverpool, Paris)")
+country = input("What is the name of your country? (eg. Spain, France)")
+print("My adress is: \n" + "Street " + street + ", " + city + ", " + country)
